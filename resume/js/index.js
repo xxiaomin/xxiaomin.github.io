263b59a32549c24446a47fd2e10e89678059ede1
xxiaomin <1218220280@qq.com>
2023-04-06 19:00:47 +0800

    Site updated: 2023-04-06 19:00:46

---
+++ resume/js/index.js
@@ -0,0 +1,16 @@
+// // 个人信息
+// let neixinxi = document.querySelector('neixinxi')
+// let arr1 = [
+//   23,
+//   '大专',
+//   '2000.09',
+//   15811666368,
+//   '广东汕尾 ',
+//   '云计算技术与应用',
+//   '共青团员',
+//   '1218220280@qq.com'
+// ]
+// let arr1 = []
+// for(let i = 0 ; i < arr1.length;i++){
+//   neixinxi.innerHTML(`<span></span>`)
+// }
